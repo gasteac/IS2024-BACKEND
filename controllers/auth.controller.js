b6c1ab3a7012cbb2314f3f9bc021af381a02a8f5
Gaston Acosta <gasteac@gmail.com>
2024-06-04 19:37:12 -0300

    Remove unnecessary code for field validation in signin controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,6 +26,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(errorHandler(404, "Email not registered"));
     }
+    
     // Compara la contraseña proporcionada con la contraseña encriptada del usuario
     const validPassword = bcryptjs.compareSync(password, validUser.password);
 
