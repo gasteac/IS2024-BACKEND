5894385169ae2a657a4ecbb46fb6f349cb649802
Gaston Acosta <gasteac@gmail.com>
2024-06-07 18:02:54 -0300

    chore: Refactor signup response in auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -108,6 +108,7 @@ export const signup = async (req, res, next) => {
     
     // Configura la respuesta con estado 201 (Creado), agrega una cookie con el token y envía el resto de los datos del usuario
     res
+    
       .status(201)
       .cookie("access_token", token, {
         httpOnly: true,
