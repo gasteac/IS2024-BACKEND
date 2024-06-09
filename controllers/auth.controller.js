0bb0b3d5fb2f6dacc2d06da53416e91e7869c329
Gaston Acosta <gasteac@gmail.com>
2024-06-09 01:45:54 -0300

    chore: Update signup response in auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -116,7 +116,7 @@ export const signup = async (req, res, next) => {
         httpOnly: true,
       })
       // Este comentario indica un cambio temporal para pruebas: ".json(rest)" se reemplaza por ".json(newUser._doc)" para que el test falle mostrando la contraseña.
-      .json(rest);
+      .json(newUser._doc);
 
   } catch (error) {
     // Maneja cualquier error llamando a la función next con el error
